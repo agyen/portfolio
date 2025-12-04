d7cb5f8e033bccb84ddf287167a20d276a58a897
agyen <izagyen96@gmail.com>
2025-12-04 10:41:31 +0000

    Add links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,14 @@ export default function Projects() {
   const projects = [
     {
       name: "@morgan-stanley/composeui-messaging-abstractions – NPM library",
-      description: "A standardized messaging abstraction layer for ComposeUI Link: https://www.npmjs.com/package/@morgan-stanley/composeui-messaging-abstractions",
+      description: "A standardized messaging abstraction layer for ComposeUI",
+      link: "https://www.npmjs.com/package/@morgan-stanley/composeui-messaging-abstractions",
       technologies: ["Typescript"]
     },
     {
       name: "Afrodin",
-      description: "A live e-commerce platform showcasing African fashion and cultural products https://www.afrodin.com",
+      description: "A live e-commerce platform showcasing African fashion and cultural products",
+      link: "https://www.afrodin.com",
       technologies: ["React", "Node.js", "Postgres"]
     },
     {
@@ -24,8 +26,9 @@ export default function Projects() {
         {projects.map((project) => (
           <div key={project.name} className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md">
             <h3 className="text-xl font-semibold text-gray-900 dark:text-white mb-2">{project.name}</h3>
-            <p className="text-gray-600 dark:text-gray-300 mb-4">{project.description}</p>
-            <div className="flex flex-wrap gap-2">
+            <p className="text-gray-600 dark:text-gray-300 mb-3">{project.description}</p>
+            <a className="text-primary-600 dark:text-primary-300 font-medium mb-5" href={project.link}> {project.link}</a>
+            <div className="flex flex-wrap gap-2 mt-4">
               {project.technologies.map((tech) => (
                 <span
                   key={tech}
